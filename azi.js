486506d2c23e7308f3e5a3431e5b1180626b4088
sherikbaev <luntik01011@gmail.com>
2024-02-15 12:45:32 +0600

    push

---
+++ azi.js
@@ -0,0 +1,11 @@
+{/* <form >
+<p><input type="text"placeholder="напишите текст"></p>
+<P><input type = "tel"></P>
+<p><input type="reset"></p>
+<p><input type="date"></p>
+<p><input type="radio"></p>
+<p><input type="checkbox"></p>
+<label for="">samsy</label>
+<input type="checkbox">
+    </form>
+   */}
